d4913c858ff59fe1a40c2f30c8d6f4240561cd63
Nick O <nicholas.oetinger@gmail.com>
2019-07-03 21:54:04 -0400

    finished Word.js functions and started game play function.

--- Word.js
+++ Word.js
@@ -1,1 +1,37 @@
 var Letter = require('./Letter.js');
+
+function Word(word) {
+
+    this.makeWord = function (word) {
+        var tempHolder = [];
+        for (var i = 0; i < word.length; i++) {
+            var currentCharacter = word[i];
+            var currentLetter = new Letter(currentCharacter);
+            currentWord.push(currentLetter);
+        }
+        return tempHolder;
+    }
+
+    this.word = this.makeWord(word);
+
+    this.display = function(){
+        var displayWord = "";
+        for (var i = 0; i , this.word.length; i++){
+            var currentLetter = this.word[i];
+            displayWord += currentLetter.display() + " ";
+        }
+        connsole.log(displayWord);
+    }
+
+    this.checkCharacter = function(char){
+        for (var i = 0; i < this.word.length; i++){
+            var currentLetter = this.word[i];
+            // console.log(currentLetter);
+            currentLetter.userGuess(char);
+        }
+        return this.display;
+    }
+
+};
+
+module.exports = Word;
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+var Letter = require('./Letter.js');
 var Word = require('./Word.js');
+var inquirer = require('inquirer');
+
+function newGame (){
+    //Word Bank
+    var wordBank = ["apple","banana","orange","strawberry"];
+    //Choose random word to guess
+    var wordToGuess = wordBank[Math.floor(Math.random() * wordBank.length)];
+    //put wordToGuess into new Word function
+    var word = new Word(wordToGuess);
+    //display the word
+    word.display();
+    //input word into guess function
+    playerGuess(word);
+};
+
+
+//User guess function
+    //Prompt for a guess/letter (inquirer)
+    //then takes in guess
+        //pass guess thru word.check function
+        //displays word
+        //repeats function until no letters left
+
+//Call new game
+newGame();
