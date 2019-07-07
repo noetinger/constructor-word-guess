17e7273dd55392f28a6c3397552d8e11216c64dd
Nick O <nicholas.oetinger@gmail.com>
2019-07-06 23:59:49 -0400

    Added wins and losses.

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const wordBank = ['dog', 'cat', 'fish'];
+const wordBank = ['apple', 'banana', 'orange', 'peach', 'strawberry', 'blackberry', 'pineapple', 'coconut'];
 const selectedWord = new Word();
 let wins = 0;
+let losses = 0;
 let displayWord;
+let guesses;
 
 function startGame() {
+    guesses = 12;
     const randomIndex = Math.floor(Math.random() * wordBank.length);
     selectedWord.populateLetters(wordBank[randomIndex]);
     showPuzzle(selectedWord);
@@ -18,24 +21,37 @@ function startGame() {
 }
 
 function askLetter() {
-    rl.question('Guess a letter...', function (guess) {
+    rl.question('Guess a letter...\n', function (guess) {
         selectedWord.guessLetter(guess);
         showPuzzle(selectedWord);
-        if (displayWord.indexOf("_") > -1) {
+        if (displayWord.indexOf("_") > -1 && guesses > 1) {
             askLetter(selectedWord);
+            guesses--;
+            console.log("You have " + guesses + " guesses remaining.")
+        } else if (guesses === 1) {
+            console.log("Sorry, you lost!")
+            console.log("You ran out of guesses")
+            losses++
+            console.log("Wins: " + wins);
+            console.log("Losses: " + losses);
+            console.log("Game will start again momentarily")
+            setTimeout(startGame, 2000);
         } else {
             console.log("You Won!")
             console.log("Game will start again momentarily")
             wins++
-            console.log(wins);
-            setTimeout(startGame, 3000);
+            console.log("Wins: " + wins);
+            console.log("Losses: " + losses);
+            setTimeout(startGame, 2000);
         };
     })
 }
 
 function showPuzzle(word) {
     displayWord = word.getPuzzle()
-    console.log(displayWord);
+    console.log("\n")
+    console.log("Name of a fruit!")
+    console.log("Fruit: " + displayWord);
 }
 
 startGame();
